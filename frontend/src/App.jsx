50052602610a1dccb1a0da23e9ebecd94328f6a4
MHarrisT <harristabassum151@gmail.com>
2026-08-27 19:42:53 +0500

    feat: added User routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import {BorwserRouter, Routes, Route, BrowserRouter} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
@@ -6,6 +8,15 @@ function App() {
 
   return (
     <>
+      <div>
+        <BrowserRouter>
+        <Routes>
+          <Route path = '/' element={}></Route>
+          <Route path = '/create' element={}></Route>
+          <Route path = '/update' element={}></Route>
+        </Routes>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
