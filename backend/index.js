63203d1962469418784be21fca3fc883e7591489
MHarrisT <harristabassum151@gmail.com>
2026-08-27 21:13:05 +0500

    feat: added user record

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const UserModel = require("./models/Users")
 const PORT = 3001
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+mongoose.connect("mongodb://127.0.0.1:27017/crud")
+
+
 
 app.listen(PORT, () => console.log("Server is Running on PORT " + PORT))
