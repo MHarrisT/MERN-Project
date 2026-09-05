4aa4ae2c44f6720cd80dccd617a0e5f37848f055
MHarrisT <harristabassum151@gmail.com>
2026-09-05 13:50:30 +0500

    chore: set up db

--- Ecommerce App/backend/server.js
+++ Ecommerce App/backend/server.js
@@ -1,8 +1,12 @@
 const app = require('./app')
 const dotenv = require('dotenv')
+const connectDatabase = require("./config/database")
 
 // Config
-dotenv.config({path: 'backend/config/config.env'})
+dotenv.config({path:'backend/config/config.env'})
+
+// Connecting to databse
+connectDatabase()
 
 app.listen(process.env.PORT,()=>{
     console.log('Server is working on PORT: ' + process.env.PORT)
