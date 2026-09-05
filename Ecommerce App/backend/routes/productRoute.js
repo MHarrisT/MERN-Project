eac71c1a1e51676632832ed1bf2fc2cda433f528
MHarrisT <harristabassum151@gmail.com>
2026-09-05 16:21:14 +0500

    feat: added productCRUD operations

--- Ecommerce App/backend/routes/productRoute.js
+++ Ecommerce App/backend/routes/productRoute.js
@@ -1,7 +1,11 @@
 const express = require("express")
-const { getAllProducts } = require("../controllers/productController")
+const { getAllProducts, getProductDetails, createProduct, updateProduct, deleteProduct } = require("../controllers/productController")
 const router = express.Router()
 
 router.route('/products').get(getAllProducts)
+router.route('/product/:id').get(getProductDetails)
+router.route('/product/new').post(createProduct)
+router.route("/product/update/:id").put(updateProduct)
+router.route("/product/delete/:id").delete(deleteProduct)
 
 module.exports = router
