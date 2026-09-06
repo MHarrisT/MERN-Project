d45f560e35c623d7c14ae2d40a5c2489af46dabc
MHarrisT <harristabassum151@gmail.com>
2026-09-06 15:57:25 +0500

    feat: search, filters, pagination

--- Ecommerce App/backend/app.js
+++ Ecommerce App/backend/app.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express()
 const errorMiddleware = require("./middleware/error")
 
+// Fix for Express 5: enable extended query parser for nested bracket notation (e.g. price[gt]=1000)
+app.set('query parser', 'extended')
+
 app.use(express.json())
 
 // route imports
--- Ecommerce App/backend/controllers/productController.js
+++ Ecommerce App/backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/productModel')
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require('../utils/apifeatures');
 
 // Create Product -- Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
@@ -13,7 +14,13 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Products
 exports.getAllProducts = catchAsyncErrors(async(req, res, next) => {
-    const products = await Product.find()
+    const resultPerPage = 5
+    const productCount = await Product.countDocuments()
+    const apiFeature = new ApiFeatures(Product.find(),req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage)
+    const products = await apiFeature.query
     res.status(200).json(
         {
             success: true,
@@ -30,7 +37,8 @@ exports.getProductDetails = catchAsyncErrors(async(req, res, next) => {
     }
     res.status(200).json({
         success: true,
-        product
+        product,
+        productCount
     })    
 })
 
